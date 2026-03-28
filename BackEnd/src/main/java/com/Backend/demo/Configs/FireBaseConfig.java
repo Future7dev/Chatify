19111a1dc75fc62bb4bed5193ea6863b1f08e3f9
Future7dev <koleypriyam8@gmail.com>
2026-03-28 16:34:11 +0530

    docker file added

--- BackEnd/src/main/java/com/Backend/demo/Configs/FireBaseConfig.java
+++ BackEnd/src/main/java/com/Backend/demo/Configs/FireBaseConfig.java
@@ -5,22 +5,27 @@ import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
 import jakarta.annotation.PostConstruct;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.io.ClassPathResource;
 
+import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 
 @Configuration
 public class FireBaseConfig {
+
     @PostConstruct
-    public void init() throws IOException{
+    public void init() throws IOException {
+
         String firebaseConfig = System.getenv("FIREBASE_CONFIG");
-        InputStream serviceAccount= new ClassPathResource(firebaseConfig).getInputStream();
+
+        InputStream serviceAccount = new ByteArrayInputStream(firebaseConfig.getBytes());
+
         FirebaseOptions options = FirebaseOptions.builder()
                 .setCredentials(GoogleCredentials.fromStream(serviceAccount))
                 .setStorageBucket("chat-app-7739a.firebasestorage.app")
                 .build();
-        if (FirebaseApp.getApps().isEmpty()){
+
+        if (FirebaseApp.getApps().isEmpty()) {
             FirebaseApp.initializeApp(options);
         }
     }
